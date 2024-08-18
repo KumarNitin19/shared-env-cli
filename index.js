22fafa0aba19a1de10bc6eb1e781b91e032c1d8a
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-19 01:40:42 +0530

    feat: firebase admin added and connected

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 
-const { initializeFirebaseApp } = require("./firebase");
-const { getDocs, collection, query } = require("firebase/firestore");
+const { db } = require("./firebase");
 
-initializeFirebaseApp();
+require("dotenv").config();
+
+const getENVData = async () => {
+  try {
+    const collectionRef = db.collection("Projects");
+    const finalData = [];
+    const projectSnap = await collectionRef.get();
+    projectSnap.forEach((project) => {
+      finalData.push(project.data());
+    });
+    console.log("------->", finalData);
+    return finalData;
+  } catch (error) {
+    console.log("Error : ", error);
+  }
+};
+
+getENVData();
