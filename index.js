05a4a5957df0deed3b89ba410fb07e2249475c9f
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-19 02:11:46 +0530

    fix: real data is written in the file function added

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
 require("dotenv").config();
-// const { db } = require("./firebase");
+const { db } = require("./firebase");
 
-// const getENVData = async () => {
-//   try {
-//     const collectionRef = db.collection("Projects");
-//     const finalData = [];
-//     const projectSnap = await collectionRef.get();
-//     projectSnap.forEach((project) => {
-//       finalData.push(project.data());
-//     });
-//     return finalData;
-//   } catch (error) {
-//     console.log("Error : ", error);
-//   }
-// };
+const getENVData = async () => {
+  try {
+    const collectionRef = db.collection("Projects");
+    const finalData = [];
+    const projectSnap = await collectionRef.get();
+    projectSnap.forEach((project) => {
+      finalData.push(project.data());
+    });
+    return finalData;
+  } catch (error) {
+    console.log("Error : ", error);
+  }
+};
 
-// getENVData();
+getENVData();
 
 const fs = require("fs");
 
@@ -33,7 +33,7 @@ const checkWeatherFileExistsOrNot = () => {
 
 const createFile = () => {
   const createStream = fs.createWriteStream(".env.local");
-  createStream.end();
+  // createStream.end();
   return createStream;
 };
 
@@ -50,7 +50,27 @@ const readFileContent = () => {
   });
 };
 
-const writeInFile = (file) => {
-  file.write("Hi, JournalDEV Users. ");
-  file.write("Thank You.");
+const writeInFile = async (file) => {
+  const dataToWrite = await getENVData();
+  if (dataToWrite && dataToWrite[0].env_variables.length) {
+    dataToWrite[0].env_variables.map((variable) => {
+      const objKey = Object.keys(variable)[0];
+      file.write(`${objKey}=${variable[objKey]}`);
+    });
+    file.end();
+  } else {
+    throw new Error("Something went wrong!!");
+  }
 };
+
+const readAndWriteFile = () => {
+  const isFilePresent = checkWeatherFileExistsOrNot();
+  if (isFilePresent) {
+    readFileContent();
+  } else {
+    const file = createFile();
+    writeInFile(file);
+  }
+};
+
+readAndWriteFile();
