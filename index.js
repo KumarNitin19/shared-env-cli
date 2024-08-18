9b62cc242f68bc3dcb894cff91ac7f3dfa264831
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-19 02:01:17 +0530

    feat: check whether file exists or not function added

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
 
-const { db } = require("./firebase");
-
 require("dotenv").config();
+// const { db } = require("./firebase");
 
-const getENVData = async () => {
-  try {
-    const collectionRef = db.collection("Projects");
-    const finalData = [];
-    const projectSnap = await collectionRef.get();
-    projectSnap.forEach((project) => {
-      finalData.push(project.data());
-    });
-    console.log("------->", finalData);
-    return finalData;
-  } catch (error) {
-    console.log("Error : ", error);
+// const getENVData = async () => {
+//   try {
+//     const collectionRef = db.collection("Projects");
+//     const finalData = [];
+//     const projectSnap = await collectionRef.get();
+//     projectSnap.forEach((project) => {
+//       finalData.push(project.data());
+//     });
+//     return finalData;
+//   } catch (error) {
+//     console.log("Error : ", error);
+//   }
+// };
+
+// getENVData();
+
+const fs = require("fs");
+
+const pathToFileOrDir = "./.env.local";
+const fileName = ".env.local";
+
+const checkWeatherFileExistsOrNot = () => {
+  if (fs.existsSync(pathToFileOrDir)) {
+    return true;
   }
+  return false;
 };
-
-getENVData();
