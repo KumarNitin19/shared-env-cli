bb2328b9d2365a165ad9948c8ebe5202c544273d
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-01-25 03:04:22 +0530

    feat: function to get logged in users email from vs code

--- index.js
+++ index.js
@@ -82,3 +82,20 @@ function startCLI() {
 
 // Start the CLI tool
 startCLI();
+
+// Fetch user GitHub session
+// const fetchGitHubSession = () => {
+//   try {
+//     const githubUsername = execSync("git config user.email").toString().trim();
+//     if (!githubUsername) {
+//       console.error("GitHub session not found. Please login via GitHub.");
+//       process.exit(1);
+//     }
+//     return githubUsername;
+//   } catch (error) {
+//     console.error("Error fetching GitHub session:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// console.log(fetchGitHubSession());
