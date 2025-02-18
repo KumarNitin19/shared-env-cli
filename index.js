32a2e77f6aef18e1f3bbf1c57ef879c4bd1a9eb7
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-18 23:16:22 +0530

    fix: console message fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const readline = require("readline");
-const fetchGitHubSession = require("./git-actions/utils");
 const fetchENVVariableForProject = require("./query");
 const { readAndWriteFile } = require("./file-actions/write-file");
+const { fetchGitHubSession } = require("./git-actions/utils");
 
 // ANSI color codes for random colors
 const colors = [
@@ -35,47 +35,46 @@ function renderList(list, selectedIndex, colorMap) {
 async function startCLI() {
   let selectedIndex = 0;
   const projectId = "0c515a83-2d3c-4ce1-8ba4-a8a2550faf73";
-  const envGroups = await fetchENVVariableForProject(projectId).then(
-    (res) => res
-  );
+  const envGroups = await fetchENVVariableForProject(projectId);
   const groupNames = envGroups?.groups?.map((group) => group?.groupName);
+  if (groupNames?.length) {
+    // Generate a random color for each item
+    const colorMap = groupNames.map((_, index) => colors[index]);
 
-  // Generate a random color for each item
-  const colorMap = groupNames.map((_, index) => colors[index]);
+    // Render the initial list
+    renderList(groupNames, selectedIndex, colorMap);
 
-  // Render the initial list
-  renderList(groupNames, selectedIndex, colorMap);
+    // Set up keypress handling
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
 
-  // Set up keypress handling
-  readline.emitKeypressEvents(process.stdin);
-  process.stdin.setRawMode(true);
-
-  process.stdin.on("keypress", (_, key) => {
-    if (key.name === "up") {
-      selectedIndex =
-        selectedIndex > 0 ? selectedIndex - 1 : groupNames.length - 1;
-      renderList(groupNames, selectedIndex, colorMap);
-    } else if (key.name === "down") {
-      selectedIndex =
-        selectedIndex < groupNames.length - 1 ? selectedIndex + 1 : 0;
-      renderList(groupNames, selectedIndex, colorMap);
-    } else if (key.name === "return") {
-      console.clear();
-      console.log(`You selected: ${groupNames[selectedIndex]}`);
-      const selectedENV = envGroups?.groups?.find(
-        (group) => group?.groupName === groupNames[selectedIndex]
-      );
-      const variables = selectedENV?.variables;
-      readAndWriteFile(variables);
-      console.log("Successfully created env.");
-      process.stdin.setRawMode(false);
-      process.stdin.pause();
-    } else if (key.ctrl && key.name === "c") {
-      process.stdin.setRawMode(false);
-      process.stdin.pause();
-      console.log("\nExited.");
-    }
-  });
+    process.stdin.on("keypress", (_, key) => {
+      if (key.name === "up") {
+        selectedIndex =
+          selectedIndex > 0 ? selectedIndex - 1 : groupNames.length - 1;
+        renderList(groupNames, selectedIndex, colorMap);
+      } else if (key.name === "down") {
+        selectedIndex =
+          selectedIndex < groupNames.length - 1 ? selectedIndex + 1 : 0;
+        renderList(groupNames, selectedIndex, colorMap);
+      } else if (key.name === "return") {
+        console.clear();
+        console.log(`You selected: ${groupNames[selectedIndex]}`);
+        const selectedENV = envGroups?.groups?.find(
+          (group) => group?.groupName === groupNames[selectedIndex]
+        );
+        const variables = selectedENV?.variables;
+        readAndWriteFile(variables);
+        console.log("Successfully added env variables.");
+        process.stdin.setRawMode(false);
+        process.stdin.pause();
+      } else if (key.ctrl && key.name === "c") {
+        process.stdin.setRawMode(false);
+        process.stdin.pause();
+        console.log("\nExited.");
+      }
+    });
+  }
 }
 
 fetchGitHubSession();
