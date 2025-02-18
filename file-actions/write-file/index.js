ce4d16a08625751ce9b8ab991020a7f82875a3e8
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-18 23:52:44 +0530

    fix: write in file fixes

--- file-actions/write-file/index.js
+++ file-actions/write-file/index.js
@@ -4,26 +4,30 @@ const { readFileContent } = require("../read-file");
 
 // adding entries to the file
 const writeInFile = async (file, env_variables) => {
-  if (env_variables.length) {
+  if (env_variables?.length) {
     env_variables.map((variable) => {
       const objKey = Object.keys(variable)[0];
       file.write(`${objKey}=${variable[objKey]}\n`);
     });
     file.end();
   } else {
-    throw new Error("Something went wrong!!");
+    console.log("No variables found");
   }
 };
 
 // creating/adding entries to the file
-const readAndWriteFile = (env_variables) => {
+const readAndWriteFile = async (env_variables) => {
   const pathToFileOrDir = "../../.env.local";
+  const fileName = ".env.local";
   const isFilePresent = checkWeatherFileExistsOrNot(pathToFileOrDir);
+  await readFileContent(fileName);
   if (isFilePresent) {
-    readFileContent();
+    const variables = await readFileContent(fileName);
+    const file = createFile(fileName);
+    if (variables?.length) {
+      writeInFile(file, variables);
+    } else writeInFile(file, env_variables);
   } else {
-    console.log("called");
-    const fileName = ".env.local";
     const file = createFile(fileName);
     writeInFile(file, env_variables);
   }
