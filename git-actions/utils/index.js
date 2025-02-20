2c6d27650890dff3467b2c8a6d8f14c6def0909b
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-20 23:43:01 +0530

    fix: loader and better console added

--- git-actions/utils/index.js
+++ git-actions/utils/index.js
@@ -10,7 +10,6 @@ const fetchGitHubSession = () => {
       console.error("GitHub session not found. Please login via GitHub.");
       process.exit(1);
     }
-    console.log(githubUsername);
     return githubUsername;
   } catch (error) {
     console.error("Error fetching GitHub session:", error.message);
@@ -28,4 +27,36 @@ const checkWeatherFileExistsOrNot = (pathname) => {
   }
 };
 
-module.exports = { fetchGitHubSession, checkWeatherFileExistsOrNot };
+const spinnerFrames = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+let spinnerIndex = 0;
+
+const blue = "\x1b[34m"; // Blue color
+const reset = "\x1b[0m"; // Reset color
+const clearLine = "\x1b[2K"; // Clears the current line
+const moveCursorToStart = "\r";
+
+const startSpinner = (message) => {
+  process.stdout.write("\n");
+  process.stdout.write("\x1B[?25l"); // Hide cursor
+
+  return setInterval(() => {
+    process.stdout.write(
+      `${moveCursorToStart}${clearLine}${blue}${spinnerFrames[spinnerIndex]} ${message}  ${reset}`
+    );
+    spinnerIndex = (spinnerIndex + 1) % spinnerFrames.length;
+  }, 100);
+};
+
+const stopSpinner = (spinner, message, isSuccess = true) => {
+  clearInterval(spinner);
+  process.stdout.write(`${moveCursorToStart}${clearLine}`); // Clear the spinner line
+  console.log(`${isSuccess ? "✅" : "❌"} ${message}\n`); // Print success/failure message
+  process.stdout.write("\x1B[?25h"); // Show cursor
+};
+
+module.exports = {
+  fetchGitHubSession,
+  checkWeatherFileExistsOrNot,
+  startSpinner,
+  stopSpinner,
+};
