b1a21f5de79cad8124fd70445983504958f80053
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-27 00:06:15 +0530

    fix: git file action fixes

--- git-actions/add-to-git-ignore/index.js
+++ git-actions/add-to-git-ignore/index.js
@@ -1,14 +1,23 @@
 const { readGitFile } = require("../../file-actions/read-file");
+const { checkWeatherFileExistsOrNot } = require("../utils");
+const fs = require("fs");
 
 // add env file to .gitignore
 const addToGitIgnore = async () => {
+  const pathToFileOrDir = "../../.gitignore";
   const fileName = ".gitignore";
-  const fileContent = await readGitFile();
-  if (fileContent.includes(fileName)) return;
-  fs.appendFile(fileName, "\n.env", function (err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
+  const isFilePresent = checkWeatherFileExistsOrNot(pathToFileOrDir);
+  if (isFilePresent) {
+    const fileContent = await readGitFile();
+    if (fileContent.includes(fileName)) return;
+    fs.appendFile(fileName, "\n.env \nvarVault.json", function (err) {
+      if (err) throw err;
+    });
+  } else {
+    fs.appendFile(fileName, ".env\nvarVault.json", function (err) {
+      if (err) throw err;
+    });
+  }
 };
 
 module.exports = addToGitIgnore;
