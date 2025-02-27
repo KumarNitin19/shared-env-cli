8b4b9eecb1efad34518e08b79fdd5ec7686108fe
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-02-28 00:26:28 +0530

    perf: all utils files and folders removed

--- file-actions/create-file/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-// creating a file
-const createFile = (fileName) => {
-  const createStream = fs.createWriteStream(fileName);
-  return createStream;
-};
-
-module.exports = createFile;
